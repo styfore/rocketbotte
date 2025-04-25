3ce1beb312797535cf6d1563fda0acf02dea5e3b
Styfore <contact@styfore.ovh>
2025-04-25 14:19:31 +0200

    test

--- src/rocketbotte/client.py
+++ src/rocketbotte/client.py
@@ -3,13 +3,17 @@ from loguru import logger
 import asyncio, aiohttp, time
 from aiohttp import BasicAuth
 from datetime import datetime, timezone
+from types import SimpleNamespace
+from yarl import URL
 
 class Bot():
+    API:str = 'api/v1'
+    
     def __init__(self, user_id:str, auth_token:str, server_url:str, delay=1):
         super().__init__()
         self.user_id = user_id
         self.auth_token = auth_token
-        self.server_url = server_url
+        self.server_url = URL(server_url)
         self.status = 'OFF'
         self.delay = delay
         self.headers = {'X-User-Id': self.user_id, 'X-Auth-Token': self.auth_token}
@@ -23,33 +27,39 @@ class Bot():
     async def __run(self):
         async with aiohttp.ClientSession(headers=self.headers) as session:
             await self.__login(session)
-            # logger.info(f'Connected as {self.me.name} on {self.server_url}')
-            self.status = 'CONNECTED'
-            
+
             # while self.status == 'CONNECTED':
-            #     last_update = datetime.now()
-            #     messages = await self.__get_messages(session, last_update)
-            #     await self.__process_messages(messages)
-            #     time.sleep(self.delay)
-                
-    async def __login(self, session:aiohttp.ClientSession):
-        async with session.get(url=self.server_url+ '/api/v1/me') as response:
-            print(response.headers)
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
-            self.me = await response.json()
-            print(self.me )
+            last_update = datetime.now()
+            messages = await self.__get_messages(session, last_update)
+            time.sleep(self.delay)
             
+                
+            
+
+            
+                
+    async def __call_api(self, session:aiohttp.ClientSession, endpoint:str,  api:str=API, **params ):
+        async with session.get(url=self.server_url/api/endpoint, params=params) as response:
+            return response.status, await response.json()
+            
+    async def __login(self, session:aiohttp.ClientSession):
+        status, response = await self.__call_api(session, 'me')
+        if status == 200:
+            self.me = response
+            self.status = 'CONNECTED'
+            logger.info(f'Logged as {self.me['username']}')
+        else:
+            resp = await response.json()
+            raise Exception(f'{resp['status']} {response.status} : unable to connect, check auth_token or user_id : {resp['message']}')
+        
+
     async def __get_messages(self, session:aiohttp.ClientSession, last_update:datetime):
-        async with session.get(self.server_url, ) as response:
+        status, channels = await self.__call_api(session, 'subscriptions.get')
+        if status == 200:
+            for channel in channels['update']:
+                print(channel)
 
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
 
-            html = await response.text()
-            print("Body:", html[:15], "...")
- 
- 
 # /api/v1/me           
     # X-User-Id
     # stringRequired
